f1559077a21ab4f2c3cf017764c76666047b982c
Kelvis Artigas <kartigas323@gmail.com>
2016-12-22 13:53:20 -0400

    quitando postgres como backend de bd y colocando sqlite para mayor comodidad

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ class Config(object):
 	DEBUG = False
 	TESTING = False
 	CSRF_ENABLED = True
-	SQLALCHEMY_DATABASE_URI = "postgresql://postgres:1164543@172.17.0.2/flask_restful"
+	#SQLALCHEMY_DATABASE_URI = "postgresql://postgres:1164543@172.17.0.2/flask_restful"
+	SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/flask_restful.db"
 	SECRET_KEY = '6468202b-463a-4c7e-88f1-a97987a75ecd'
 
 class ProductionConfig(Config):
